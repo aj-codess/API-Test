cd4acb48e8cb9cc607c569c383ed8db10c574ed9
AJ <121802680+aj-codess@users.noreply.github.com>
2025-04-19 21:24:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import caller from "./dependencies/caller.js";
 
-caller("post","/auth/login",JSON.stringify({"email":"jagyei339@gmail.com","password":"crypto_2_get"}));
+caller("post","/auth/login",JSON.stringify({"email":"jagyei339@gmail.com","password":"dfvnrfk32432df*}));
